8186003f8ee8ca3a3186fde519c86c0d1b99377f
Mike Doe <me@mrsum.ru>
2023-08-31 12:10:54 +0500

    [nvim] fix indents by editorconfig

--- config/nvim/lua/mrsum/nvim.lua
+++ config/nvim/lua/mrsum/nvim.lua
@@ -1,44 +1,44 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 local plugins_list = {
 
-	-- themes
-	{ import = "mrsum.themes.catppuccin" },
+  -- themes
+  { import = "mrsum.themes.catppuccin" },
 
-	-- core
-	{ import = "mrsum.core.autocmds" },
-	{ import = "mrsum.core.options" },
-	{ import = "mrsum.core.keymaps" },
+  -- core
+  { import = "mrsum.core.autocmds" },
+  { import = "mrsum.core.options" },
+  { import = "mrsum.core.keymaps" },
 
-	-- editor plugins
-	{ import = "mrsum.plugins.editor.tabby" },
-	{ import = "mrsum.plugins.editor.neoscroll" },
-	{ import = "mrsum.plugins.editor.lualine" },
-	{ import = "mrsum.plugins.editor.neotree" },
-	{ import = "mrsum.plugins.editor.treesitter" },
-	{ import = "mrsum.plugins.editor.cmp" },
-	{ import = "mrsum.plugins.editor.fzf" },
-	{ import = "mrsum.plugins.editor.mini" },
+  -- editor plugins
+  { import = "mrsum.plugins.editor.tabby" },
+  { import = "mrsum.plugins.editor.neoscroll" },
+  { import = "mrsum.plugins.editor.lualine" },
+  { import = "mrsum.plugins.editor.neotree" },
+  { import = "mrsum.plugins.editor.treesitter" },
+  { import = "mrsum.plugins.editor.cmp" },
+  { import = "mrsum.plugins.editor.fzf" },
+  { import = "mrsum.plugins.editor.mini" },
 
-	-- lsp
-	{ import = "mrsum.plugins.lsp" },
+  -- lsp
+  { import = "mrsum.plugins.lsp" },
 
-	-- git
-	{ import = "mrsum.plugins.git.diffview" },
-	{ import = "mrsum.plugins.git.gitsigns" },
+  -- git
+  { import = "mrsum.plugins.git.diffview" },
+  { import = "mrsum.plugins.git.gitsigns" },
 }
 
 require("lazy").setup(plugins_list)
