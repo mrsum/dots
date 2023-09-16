28ac7df4ec1e097081c8abb72e236aaad158c16e
Mike Doe <me@mrsum.ru>
2023-09-16 13:24:15 +0500

    [nvim] add wilder to setup

--- config/nvim/lua/mrsum/core/autocmds.lua
+++ config/nvim/lua/mrsum/core/autocmds.lua
@@ -5,57 +5,35 @@ return {
     local _autocmd = vim.api.nvim_create_autocmd
     local _augroup = vim.api.nvim_create_augroup
 
-    _augroup("YankHighlight", { clear = true })
-
-    -- Remove whitespace on save
-    _autocmd("BufWritePre", {
-      pattern = "",
-      command = ":%s/\\s\\+$//e",
-    })
-
-    -- Don't auto commenting new lines
-    _autocmd("BufEnter", {
-      pattern = "",
-      command = "set fo-=c fo-=r fo-=o",
-    })
-
-    -- Disable line length marker
-    _augroup("setLineLength", { clear = true })
-    _autocmd("Filetype", {
-      group = "setLineLength",
-      pattern = { "text", "markdown", "html", "xhtml", "javascript", "typescript" },
-      command = "setlocal cc=0",
-    })
-
-    -- Open a Terminal on the right tab
-    _autocmd("CmdlineEnter", {
-      command = "command! Term :botright vsplit term://$SHELL",
-    })
-
-    -- Enter insert mode when switching to terminal
-    _autocmd("TermOpen", {
-      command = "setlocal listchars= nonumber norelativenumber nocursorline",
-    })
-
-    _autocmd("TermOpen", {
-      pattern = "",
-      command = "startinsert",
-    })
-
-    -- Close terminal buffer on process exit
-    _autocmd("BufLeave", {
-      pattern = "term://*",
-      command = "stopinsert",
-    })
-
     _autocmd("TextYankPost", {
       pattern = "*",
       callback = function()
         vim.highlight.on_yank()
       end,
-      group = vim.api.nvim_create_augroup("YankHighlight", {
-        clear = true,
-      }),
+      group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+    })
+
+    _autocmd("VimResized", {
+      group = vim.api.nvim_create_augroup("ResizeSplit", { clear = true }),
+      callback = function()
+        vim.cmd("tabdo wincmd =")
+      end,
+    })
+
+    _autocmd("FileType", {
+      pattern = "make",
+      callback = function()
+        vim.opt_local.expandtab = false
+      end,
+    })
+
+    _autocmd("TermOpen", {
+      pattern = "*",
+      callback = function()
+        vim.opt_local.number = false
+        vim.opt_local.relativenumber = false
+        vim.cmd("startinsert!")
+      end,
     })
   end,
 }
