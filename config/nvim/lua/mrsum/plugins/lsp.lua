0bb533a8e2244b99db7f70370112d46bea79433e
Mike Doe <me@mrsum.ru>
2024-07-05 14:47:30 +0500

    [nvim] fix diagnostic line autocommand

--- config/nvim/lua/mrsum/plugins/lsp.lua
+++ config/nvim/lua/mrsum/plugins/lsp.lua
@@ -39,49 +39,11 @@ return {
 
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-      virtual_text = {
-        prefix = "",
-        spacing = 0,
-      },
-      signs = true,
-      underline = true,
-    })
-
     vim.diagnostic.config({
       float = {
         border = "rounded",
       },
     })
-
-    -- symbols for autocomplete
-    vim.lsp.protocol.CompletionItemKind = {
-      "   (Text) ",
-      "   (Method)",
-      "   (Function)",
-      "   (Constructor)",
-      " ﴲ  (Field)",
-      "[] (Variable)",
-      "   (Class)",
-      " ﰮ  (Interface)",
-      "   (Module)",
-      " 襁 (Property)",
-      "   (Unit)",
-      "   (Value)",
-      " 練 (Enum)",
-      "   (Keyword)",
-      "   (Snippet)",
-      "   (Color)",
-      "   (File)",
-      "   (Reference)",
-      "   (Folder)",
-      "   (EnumMember)",
-      " ﲀ  (Constant)",
-      " ﳤ  (Struct)",
-      "   (Event)",
-      "   (Operator)",
-      "   (TypeParameter)",
-    }
 
     -- install lsp language from list
     mason.setup({
