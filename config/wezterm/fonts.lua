a28c8331bcca3c3608c56069f01116bb0efa9154
Mike Doe <me@mrsum.ru>
2024-07-10 22:05:18 +0500

    [wezterm] alomost perfect terminal emulator

--- config/wezterm/fonts.lua
+++ config/wezterm/fonts.lua
@@ -5,9 +5,10 @@ local Fonts = {}
 function Fonts.setup(config)
   config.font = wezterm.font_with_fallback({
     family = "JetBrainsMonoNL Nerd Font Mono",
-    weight = "Bold",
+    weight = "Medium",
   })
 
+  config.font_size = 14
   config.line_height = 1.2
   config.underline_position = "-3pt"
   config.underline_thickness = "100%"
