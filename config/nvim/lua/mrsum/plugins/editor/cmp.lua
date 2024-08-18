5a573c1fa548a78ec67e7427436b67c8e01f0e79
Mike Doe <me@mrsum.ru>
2024-08-18 13:38:25 +0500

    [nvim] remove duplicate deps

--- config/nvim/lua/mrsum/plugins/editor/cmp.lua
+++ config/nvim/lua/mrsum/plugins/editor/cmp.lua
@@ -5,8 +5,6 @@ return {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-path",
     "hrsh7th/cmp-nvim-lua",
     "hrsh7th/cmp-cmdline",
     "hrsh7th/cmp-nvim-lsp-document-symbol",
