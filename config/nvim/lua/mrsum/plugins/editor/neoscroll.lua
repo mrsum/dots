abc4ca4baaf18a1c0ea7264e18e09ea0028cda54
Mike Doe <me@mrsum.ru>
2024-09-02 16:16:56 +0500

    [nvim] change scroll duration time

--- config/nvim/lua/mrsum/plugins/editor/neoscroll.lua
+++ config/nvim/lua/mrsum/plugins/editor/neoscroll.lua
@@ -7,33 +7,13 @@ return {
       return
     end
 
+    -- up and down for Ctrl+
     local keymap = {
       ["<C-Up>"] = function()
-        neoscroll.ctrl_u({ duration = 250 })
+        neoscroll.ctrl_u({ duration = 150 })
       end,
       ["<C-Down>"] = function()
-        neoscroll.ctrl_d({ duration = 250 })
-      end,
-      ["<C-b>"] = function()
-        neoscroll.ctrl_b({ duration = 450 })
-      end,
-      ["<C-f>"] = function()
-        neoscroll.ctrl_f({ duration = 450 })
-      end,
-      ["<C-y>"] = function()
-        neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 })
-      end,
-      ["<C-e>"] = function()
-        neoscroll.scroll(0.1, { move_cursor = false, duration = 100 })
-      end,
-      ["zt"] = function()
-        neoscroll.zt({ half_win_duration = 250 })
-      end,
-      ["zz"] = function()
-        neoscroll.zz({ half_win_duration = 250 })
-      end,
-      ["zb"] = function()
-        neoscroll.zb({ half_win_duration = 250 })
+        neoscroll.ctrl_d({ duration = 150 })
       end,
     }
     local modes = { "n", "v", "x" }
