2365f46db4ff8f764b652f128bee5579cfdcba8b
Mike Doe <me@mrsum.ru>
2026-04-29 18:36:05 +0500

    bump version add exclude to fzf-lua

--- config/nvim/lua/mrsum/plugins/editor/fzf.lua
+++ config/nvim/lua/mrsum/plugins/editor/fzf.lua
@@ -1,5 +1,25 @@
 return {
   "ibhagwan/fzf-lua",
   name = "_.mrsum.plugins.editor.fzf",
-  config = function() end,
+  config = function()
+    local setup, plugin = pcall(require, "fzf-lua")
+    if not setup then
+      return
+    end
+
+    plugin.setup({
+      file_ignore_patterns = {
+        "node_modules",
+        "dist",
+        ".next",
+        ".git",
+        ".gitlab",
+        "build",
+        "target",
+        "package-lock.json",
+        "pnpm-lock.yaml",
+        "yarn.lock",
+      },
+    })
+  end,
 }
